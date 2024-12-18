3af51ee47bcc8a1e4696e74c9a55e855bb3d762c
Kum1ta <eddydhj@gmail.com>
2024-12-19 00:28:34 +0100

    starting database

--- server/requirements/node_server/server/src/Database.js
+++ server/requirements/node_server/server/src/Database.js
@@ -6,7 +6,7 @@
 /*   By: edbernar <edbernar@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 16:54:56 by edbernar          #+#    #+#             */
-/*   Updated: 2024/12/17 15:00:04 by edbernar         ###   ########.fr       */
+/*   Updated: 2024/12/19 00:27:17 by edbernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,12 +33,50 @@ class Database
 		this.pool.getConnection().then(conn => {
 			console.log('Connected to database');
 			conn.end();
+			this.initTables();
 			this.ready = true;
 		}).catch(err => {
 			console.log('Error connecting to database');
 			setTimeout(() => {
 				this.tryConnection();
 			}, 1000);
+		});
+	}
+
+	initTables()
+	{
+		this.pool.getConnection().then((conn) => {
+			conn.query(`CREATE TABLE IF NOT EXISTS accounts (
+				id INTEGER PRIMARY KEY,
+				email VARCHAR(320),
+				password VARCHAR(50)
+			)`);
+			conn.query(`CREATE TABLE IF NOT EXISTS users_info (
+				id INTEGER PRIMARY KEY,
+				user_id INT,
+				first_name VARCHAR(50),
+				last_name VARCHAR(50),
+				nickname VARCHAR(50),
+				date_of_birth DATE,
+				sexe VARCHAR(1),
+				orientation VARCHAR(1),
+				bio VARCHAR(500),
+				FOREIGN KEY(user_id) REFERENCES accounts(id) ON DELETE CASCADE
+			)`);
+			conn.query(`CREATE TABLE IF NOT EXISTS users_tags (
+				id INTEGER PRIMARY KEY,
+				user_id INT,
+				tag VARCHAR(32),
+				FOREIGN KEY(user_id) REFERENCES accounts(id) ON DELETE CASCADE
+			)`);
+			conn.query(`CREATE TABLE IF NOT EXISTS users_images (
+				id INTEGER PRIMARY KEY,
+				user_id INT,
+				local_url VARCHAR(64),
+				FOREIGN KEY(user_id) REFERENCES accounts(id) ON DELETE CASCADE
+			)`);
+			conn.release();
+			conn.end();
 		});
 	}
 
