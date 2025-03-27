783442faac94f60c74e12ceb60844cbcd3c107cc
edbernar <eddydhj@gmail.com>
2025-03-27 13:00:09 +0100

    fix crash

--- server/requirements/node_server/server/src/PostRequest.js
+++ server/requirements/node_server/server/src/PostRequest.js
@@ -227,7 +227,7 @@ class PostRequest
 			return (res.send(JSON.stringify({error: "Invalid date of birth"})));
 		const birthDate = new Date(req.body.date_of_birth);
 		const today = new Date();
-		const age = today.getFullYear() - birthDate.getFullYear();
+		let age = today.getFullYear() - birthDate.getFullYear();
 		const monthDiff = today.getMonth() - birthDate.getMonth();
 		if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate()))
 			age--;
@@ -562,7 +562,7 @@ class PostRequest
 			return (res.send(JSON.stringify({error: "Invalid date of birth"})));
 		const birthDate = new Date(req.body.date_of_birth);
 		const today = new Date();
-		const age = today.getFullYear() - birthDate.getFullYear();
+		let age = today.getFullYear() - birthDate.getFullYear();
 		const monthDiff = today.getMonth() - birthDate.getMonth();
 		if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate()))
 			age--;
